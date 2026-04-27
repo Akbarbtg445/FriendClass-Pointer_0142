e91c6c701d811a46a121516f8cd3a50e14e013b7
M.Ridho Akbar <akbarbtg445@gmail.com>
2026-04-27 11:10:33 +0700

    membuat input

--- tugas.cpp
+++ tugas.cpp
@@ -6,13 +6,24 @@ using namespace std;
 class Belahketupat;
 
 class LayangLayang {
-    int d1,d2
+    private:
+    int d1,d2,s1,s2;
 
 public:
+void input() {
+    cout << "--Input Layang-Layang--"<<endl;
+    cout << "Masukan Diagonal 1:"; cin >> d1;
+    cout << "Masukan Diagonal 2:"; cin >> d2;
+    cout << "Masukan Sisi Miring 1:"; cin >> s1;
+    cout << "Masukan Sisi Miring 2:"; cin >> s2;
+}
     double hitungkeliling() {
-
+        return 2 * (s1 + s2);
     }
     double hitungluas() {
         return 0.5 * d1 * d2;
     }
+void display() {
+    
+}
 };
