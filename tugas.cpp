59b9815158f1fe6382de8b3ee80aa55f8e9cb6d7
M.Ridho Akbar <akbarbtg445@gmail.com>
2026-04-27 12:08:32 +0700

    perbaikan pada fungsi belah kettupat

--- tugas.cpp
+++ tugas.cpp
@@ -28,41 +28,42 @@ void display() {
     cout << "Luas:" << hitungluas() << endl;
     cout << "keliling:" << hitungkeliling() << endl;
 }
-friend class Belahketupat;
 
+friend class Belahketupat;
+};
 class Belahketupat {
     private:
     int d1,d2,s;
 
 public:
-void input() {
-    cout << "--Input BelahKetupat--"<<endl;
+void inputData() {
     cout << "Masukan Diagonal 1:"; cin >> d1;
     cout << "Masukan Diagonal 2:"; cin >> d2;
     cout << "Masukan Sisi:"; cin >> s;
 }
-    double hitungkeliling() {
+    double keliling() {
         return 4 * s;
     }
     double hitungluas() {
         return 0.5 * d1 * d2;
     }
-void display() {
+void tampilkankeliling() {
     cout << "Hasil Belahketupat" << endl;
     cout << "Luas:" << hitungluas() << endl;
-    cout << "keliling:" << hitungkeliling() << endl;
+    cout << "keliling Belah Ketupat:" << keliling << endl;
 }
 };
 
 int main() {
     LayangLayang ll;
-    Belahketupat bk;
+    Belahketupat objectBelah;
+
+    cout << "--Input data belah ketupat--" << endl;
+    objectBelah.inputData();
 
     ll.input();
-    bk.input();
 
     ll.display();
-    bk.display();
 
     return 0;
 }
