637ba89a52d0262ed5325c3acf02997fa2f24452
M.Ridho Akbar <akbarbtg445@gmail.com>
2026-04-27 11:20:54 +0700

    membuat belahketupat

--- tugas.cpp
+++ tugas.cpp
@@ -28,5 +28,37 @@ void display() {
     cout << "Luas:" << hitungluas() << endl;
     cout << "keliling:" << hitungkeliling() << endl;
 }
+class Belahketupat {
+    private:
+    int d1,d2,s;
+
+public:
+void input() {
+    cout << "--Input BelahKetupat--"<<endl;
+    cout << "Masukan Diagonal 1:"; cin >> d1;
+    cout << "Masukan Diagonal 2:"; cin >> d2;
+    cout << "Masukan Sisi:"; cin >> s;
+}
+    double hitungkeliling() {
+        return 4 * s;
+    }
+    double hitungluas() {
+        return 0.5 * d1 * d2;
+    }
+void display() {
+    cout << "Hasil Belahketupat" << endl;
+    cout << "Luas:" << hitungluas() << endl;
+    cout << "keliling:" << hitungkeliling() << endl;
+}
 friend class Belahketupat;
 };
+
+int main() {
+    LayangLayang ll;
+    Belahketupat bk;
+
+    ll.input();
+    bk.input();
+
+    
+}
